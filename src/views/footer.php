d3950a048eb0cdcb7974ec935d0626b727293a72
lucasbrasantos <lucasbrasantos@gmail.com>
2022-11-15 19:09:28 -0300

    update views

--- src/views/footer.php
+++ src/views/footer.php
@@ -3,10 +3,12 @@
 
         
 <!-- less -->
+<script src="https://cdn.jsdelivr.net/npm/less@4" ></script>
+
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/less@4" ></script>
+
 
 <script src="../../public/assets/js/show-pwd.js"></script>
 
--- src/views/header.php
+++ src/views/header.php
@@ -5,25 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>sistema login</title>
+        
         <link rel="shortcut icon" href="../../public/assets/imgs/favicon.png" type="image/x-icon">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-        <link rel="stylesheet/less" type="text/css" href="../../public/assets/css/style.less"/>
-
+                
     </head>
     <body>
         
-        <div class="container">
-            <nav id="nav">
-                <h1 class="navTitle">Sistema de login</h1>
-
-                <div class="buttons">
-
-                    <a href="../../public/index.php">home</a>
-                    <a href="../login/login.php">logar</a>
-                    <a href="../register/register.php">cadastrar</a>
-                    <a href="#">sla</a>
-                    <a href="#">bla</a>
-
-                </div>
-
-            </nav>
+        <div class="container">            
