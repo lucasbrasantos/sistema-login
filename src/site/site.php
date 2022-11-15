2d7673ddd78f4b4670d0851bba449b03e311abc6
lucasbrasantos <lucasbrasantos@gmail.com>
2022-11-15 19:07:50 -0300

    fix link path issues

---
+++ src/site/site.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../views/header.php';
+require '../views/nav2.php';
+
+session_start();
+?>
+
+<!-- html -->
+
+
+<body>
+    
+<h2>eae</h2>
+
+</body>
+
+
+<!--  -->
+
+<?php
+
+require '../views/footer.php';
+
+?>
---
+++ src/views/nav1.php
@@ -0,0 +1,17 @@
+<head>
+    <link rel="stylesheet/less" type="text/css" href="../public/assets/css/style.less"/>
+</head>
+<nav id="nav">
+    <h1 class="navTitle">Sistema de login</h1>
+
+    <div class="buttons">
+
+        <a href="#">sla</a>
+        <a href="./index.php">home</a>
+        <a href="../src/login/login.php">logar</a>
+        <a href="../src/register/register.php">cadastrar</a>
+        <ion-icon name="person-circle-outline" class="nav-profile-icon" ></ion-icon>
+
+    </div>
+
+</nav>
